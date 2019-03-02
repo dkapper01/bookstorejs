e865c5549af90d85876dabceecfce63fabf74f60
Daniel Kapper <dkapper01@gmail.com>
2019-03-02 02:26:36 -0500

    refactor

--- js/getAuthor.js
+++
@@ -1,8 +0,0 @@
-// const url = "https://s3.amazonaws.com/api-fun/author.json";
-// fetch(url)
-//   .then(res => {
-//     return res.json();
-//   }).then(data => {
-//     console.log(data); 
-//   })
- 
--- js/getBooks.js
+++ js/getBooks.js
@@ -3,21 +3,19 @@ fetch(url)
   .then(res => {
     return res.json();
   })
-  .then(test => {
+  .then(dataApi => {
     let booksOutput = `
       <div class="image-banner my-4" style="height:270px; padding-top:4rem">
         <div class="col-md-12 text-center px-0">
-          <h1 class="display-4 font-italic text-white">${test.data.author}</h1>
-          <p class="lead my-3">Birthday: ${test.data.birthday}</p>
-          <p class="lead my-3">Birthday: ${test.data.birthPlace}</p>
+          <h1 class="display-4 font-italic text-white">${dataApi.data.author}</h1>
+          <p class="lead my-3">Birthday: ${dataApi.data.birthday}</p>
+          <p class="lead my-3">Birthday: ${dataApi.data.birthPlace}</p>
         </div>
       </div>
       <div class="container">
       `;
-    for (let i = 0; i < test.data.books.length; i++) {
-      let book = test.data.books[i];
-      let closeDivNow = 3;
-
+    for (let i = 0; i < dataApi.data.books.length; i++) {
+      let book = dataApi.data.books[i];
       if (i % 2 == 0 || i == 0) {
         booksOutput += '<div class="row mb-2 ml-0 mr-0">';
       }
@@ -26,11 +24,11 @@ fetch(url)
         <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-lg h-md-250 position-relative">
           <div class="col p-4 d-flex flex-column position-static">
             <strong class="d-inline-block mb-2 text-warning">Kids</strong>
-            <h4 class="mb-0 block-with-title">${book.title}</h4>
+            <h4 class="mb-0 hide-text-title">${book.title}</h4>
             <div class="mb-3 mt-1 text-muted">Published: ${
               book.PublishDate
             }</div>
-            <p class="block-with-text card-text mb-auto">This is a wider card with text below as a natural lead-in to additional content.text below as a natural lead-in to additional 
+            <p class="hide-text-summary card-text mb-auto">This is a wider card with text below as a natural lead-in to additional content.text below as a natural lead-in to additional 
             </p>
 
             <a href="${
